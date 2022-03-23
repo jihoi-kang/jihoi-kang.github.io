97cbf1683c2e8a797e2d9fb52636472a30a185e9
jihoi-kang <jihoi.kang@gmail.com>
2022-03-23 14:02:25 +0900

    add config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://jihoi-kang.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'jihoi-kang', // Usually your GitHub org/user name.
+  projectName: 'jihoi-kang.github.io', // Usually your repo name.
 
   presets: [
     [
